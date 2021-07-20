6c4fc98140165ad8007e91cadf449c1f236ca575
Rhys Johns <rhysjohns@pooledenergy.com>
2021-07-20 22:16:06 +1000

    - Added inital rough tactic file reading

--- minerva-server.go
+++ minerva-server.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+var testTactic = `
+tacticname: NormalForm
+packages:
+  - name: pooledenergy-hello-world
+    version: 2.0.1
+`
+
+type Package struct {
+	Name    string
+	Version string
+}
+
+type Tactic struct {
+	TacticName string
+	Packages   []Package
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	testYaml := Tactic{}
+
+	err := yaml.Unmarshal([]byte(testTactic), &testYaml)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("--- T:\n%v\n\n", testYaml)
+
+	formattedYaml, err := yaml.Marshal(&testYaml)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("--- testYaml dump:\n%s\n\n", string(formattedYaml))
 }
