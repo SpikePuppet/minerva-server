23f4c92f1d872aea2a08cc465cc915c15017fc61
Rhys Johns <rhysjohns@pooledenergy.com>
2021-07-20 22:28:05 +1000

    - Can read in a YAML file and update it, then write it out again

--- minerva-server.go
+++ minerva-server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 
 	"gopkg.in/yaml.v2"
@@ -27,10 +28,16 @@ type Tactic struct {
 func main() {
 	testYaml := Tactic{}
 
-	err := yaml.Unmarshal([]byte(testTactic), &testYaml)
+	tacticFile, err := ioutil.ReadFile("tactic.yaml")
 	if err != nil {
 		log.Fatalf("error: %v", err)
 	}
+
+	err = yaml.Unmarshal([]byte(tacticFile), &testYaml)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	testYaml.Packages = append(testYaml.Packages, Package{Name: "postgres-hello-world", Version: "1.4.1"})
 	fmt.Printf("--- T:\n%v\n\n", testYaml)
 
 	formattedYaml, err := yaml.Marshal(&testYaml)
@@ -38,4 +45,9 @@ func main() {
 		log.Fatalf("error: %v", err)
 	}
 	fmt.Printf("--- testYaml dump:\n%s\n\n", string(formattedYaml))
+
+	err = ioutil.WriteFile("tactic.yaml", []byte(formattedYaml), 0644)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
 }
